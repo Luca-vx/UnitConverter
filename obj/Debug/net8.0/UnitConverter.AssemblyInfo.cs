9f54647d4754c208ae0feff844a0b4ee2068ae9d
Lucas <lucas.silva.me2@gmail.com>
2025-01-30 11:20:36 -0300

    NS

--- obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af7663d3dc85415d89d3cceb49dfa5b12a2a6c6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43cac9502713c6a1ce7cab39b04f656545ccd09d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
