2746e24b0c5144b29ded74775ce9f23adf687b2d
Lucas <lucas.silva.me2@gmail.com>
2025-01-30 23:24:37 -0300

    aprendendo css

--- obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnitConverter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f54647d4754c208ae0feff844a0b4ee2068ae9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4dedccf741d32ba7faf31bae8b58a09fdaf87bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
